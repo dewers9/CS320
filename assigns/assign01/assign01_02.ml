8a4d0ef6f9d29345dc206cea54b45875d0a16d52
dewers9 <dewers@bu.edu>
2024-02-01 22:34:06 -0500

    fixing 2.2

--- assigns/assign01/assign01_02.ml
+++ assigns/assign01/assign01_02.ml
@@ -16,10 +16,12 @@
 
  let is_perfect (n : int) : bool =
   let rec sum_divisors i acc =
-    if i = 1 then acc + 1
-    else if n mod i = 0 then sum_divisors (i - 1) (acc + i + (if n / i <> i then n / i else 0))
-    else sum_divisors (i - 1) acc
+    if i = 1 then 
+      acc + 1
+    else if n mod i = 0 then 
+      sum_divisors (i - 1) (acc + i + (if n / i <> i then n / i else 0))
+    else 
+      sum_divisors (i - 1) acc
   in
   let sqrt_n = int_of_float (sqrt (float_of_int n)) in
-  (* Subtract n from the sum of divisors since we want proper divisors only *)
-  sum_divisors sqrt_n 0 - n = n
+    sum_divisors sqrt_n 0 - n = n
