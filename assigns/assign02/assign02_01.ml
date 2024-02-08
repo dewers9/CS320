cf0267f956f113b591d499eb4f62940704cdec71
dewers9 <dewers@bu.edu>
2024-02-07 19:20:12 -0500

    maybe this will help

--- assigns/assign02/assign02_01.ml
+++ assigns/assign02/assign02_01.ml
@@ -21,8 +21,39 @@ type int_or_string
   | String of string
 
 let rec convert (l : int_or_string list) : int_list_or_string_list list =
-  assert false
+  let rec go (lst : int_or_string list) (i_acc : int list) (s_acc : string list) (acc : int_list_or_string_list list ): int_list_or_string_list list =
+    match lst with
+    | [] -> 
+      if List.is_empty i_acc then
+        acc @ [StringList (s_acc)]
+      else
+        acc @ [IntList (i_acc)]
 
-let test_in = [String "call"; String "doll"; String "ere"; Int 2; Int 3; String "a"; String "b"; Int 4; String "call"; String "doll"; String "ere"]
-let test_out = [StringList ["call";"doll";"ere"]; IntList [2;3]; StringList ["a";"b"]; IntList [4]; StringList ["call";"doll";"ere"]]
+    | h :: t ->
+      if List.is_empty i_acc && List.is_empty s_acc then
+        match h with 
+        | Int n ->
+          go t [n] [] []
+        | String s ->
+          go t [] [s] []
+      
+
+      else if List.is_empty i_acc then
+        match h with 
+        | Int n ->
+          go t [n] [] (acc @ [StringList (s_acc)])
+        | String s ->
+          go t [] (s_acc @ [s]) (acc)
+      
+      else
+        match h with 
+        | Int n ->
+          go t (i_acc @ [n]) [] (acc)
+        | String s ->
+          go t [] [s] (acc @ [IntList (i_acc)])
+    in
+  go l [] [] []
+
+let test_in = [String "call"; String "doll"; String "ere"; Int 200; Int 3; String "a"; String "b"; Int 4; String "call"; String "doll"; String "ere"]
+let test_out = [StringList ["call";"doll";"ere"]; IntList [200;3]; StringList ["a";"b"]; IntList [4]; StringList ["call";"doll";"ere"]]
 let _ = assert (convert test_in = test_out)
